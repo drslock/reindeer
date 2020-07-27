395c313c1e7e6385241906f132b84e1e95a0add9
Simon Lock <simon.lock@bristol.ac.uk>
2020-07-27 22:44:59 +0100

    Added name validator

--- src/main/java/com/spe/reindeer/ReindeerApplication.java
+++ src/main/java/com/spe/reindeer/ReindeerApplication.java
@@ -2,12 +2,20 @@ package com.spe.reindeer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ReindeerApplication {
+
+	private static final List<String> validNames = Arrays.asList(new String[]{"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"});
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReindeerApplication.class, args);
 	}
 
+	public boolean isValidName(String name)
+	{
+		return validNames.contains(name);
+	}
 }
--- src/test/java/com/spe/reindeer/ReindeerApplicationTests.java
+++ src/test/java/com/spe/reindeer/ReindeerApplicationTests.java
@@ -2,12 +2,19 @@ package com.spe.reindeer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ReindeerApplicationTests {
 
+	@Autowired
+	ReindeerApplication application;
+
 	@Test
 	void contextLoads() {
+		assertThat(application.isValidName("Donner")).isTrue();
+//		assertThat(application.isValidName("Rudolph")).isTrue();
 	}
 
 }
