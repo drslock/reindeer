0cb9da23c67cfc74ae23feae44d9ebfb932f82bf
Simon Lock <simon.lock@bristol.ac.uk>
2020-07-27 18:04:57 +0100

    Initial Commit

---
+++ src/main/java/com/spe/reindeer/ReindeerApplication.java
@@ -0,0 +1,13 @@
+package com.spe.reindeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReindeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReindeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spe/reindeer/ReindeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spe.reindeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReindeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
