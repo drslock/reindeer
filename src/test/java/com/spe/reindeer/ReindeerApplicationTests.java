04a8c5520d325619a1a854f11deaf5f838313038
Simon Lock <simon.lock@bristol.ac.uk>
2020-07-28 10:39:24 +0100

    Switched to jupiter assertions

--- src/test/java/com/spe/reindeer/ReindeerApplicationTests.java
+++ src/test/java/com/spe/reindeer/ReindeerApplicationTests.java
@@ -3,18 +3,18 @@ package com.spe.reindeer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ReindeerApplicationTests {
 
 	@Autowired
-	ReindeerApplication application;
+	ReindeerApplication app;
 
 	@Test
 	void contextLoads() {
-		assertThat(application.isValidName("Donner")).isTrue();
-		assertThat(application.isValidName("Rudolph")).isTrue();
+		assertTrue(app.isValidName("Donner"), "Check 'Donner' is valid");
+		assertTrue(app.isValidName("Rudolph"), "Check 'Rudolph' is valid");
 	}
 
 }
